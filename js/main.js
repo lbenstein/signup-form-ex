6e0631fceaa90f008a77d3c98e75b3947c964696
Lara Benstein <larabenstein@Laras-Mac-Pro.local>
2019-12-12 19:59:19 -0500

    getting closer but not cigar

--- js/main.js
+++ js/main.js
@@ -7,23 +7,36 @@
 - If input fields don’t meet certain criteria, then show the warning markup
 - Add an event listener that will run your function when certain user actions are taken
 (firstname == null || firstname == "")
+
+- Let’s pull all of your variables _outside_ of your `isEmpty` function. We don’t want to be re-declaring those
+variable every time that function is run, so we should move them into the global scope and utilize them inside
+the function as necessary.
+-  Rather than store each individual input field in it’s own variable, think about how can we can store _all_ of
+our non-submit input types into a single variable (hint: what do we get when we use `querySelectorAll`?). This
+will set us up to be able to loop through each input field, check for a user input value, and then do some stuff.
+- For now, let’s just focus on adding an event listener that fires when a user clicks the submit button. When
+that occurs, we should check for valid values in our input fields and display our warning messages accordingly.
+-Maybe even before you try the conditionals to check your input fields, simply try showing your hidden warnings
+when someone clicks the submit button. Then work from there.
+
+if (firstname.value == null || firstname.value.length === 0) {
+  firstnameW.style.visibility = "visible";
+}
 */
 
+var firstname = document.getElementById('first');
+var lastname = document.getElementById('last');
+var email = document.getElementById('email');
+var password = document.getElementById('pass');
+var firstnameW = document.getElementById('firstname-warning');
+var lastnameW = document.getElementById('lastname-warning');
+var emailW = document.getElementById('email-warning');
+var passwordW = document.getElementById('password-warning');
+var mySubmit = document.getElementById('submit');
 
-
-function isEmpty(value){
-
-  var firstname = document.getElementById('first');
-  var lastname = document.getElementById('last');
-  var email = document.getElementById('email');
-  var password = document.getElementById('pass');
-  var firstnameW = document.getElementById('firstname-warning');
-  var lastnameW = document.getElementById('lastname-warning');
-  var emailW = document.getElementById('email-warning');
-  var passwordW = document.getElementById('password-warning');
-
-
-    if (firstname.value == null || firstname.value.length === 0) {
-      firstnameW.style.visibility = "visible";
-    }
+function isEmpty(event){
+  firstname.classList.add('input-warning');
+  firstnameW.style.display = "block";
 }
+
+mySubmit.addEventListener('click', isEmpty, false);
